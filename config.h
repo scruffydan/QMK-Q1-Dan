d44634d32d402c681f258ff2e16119dfda5e7632
Dan Moutal <dan@moutal.ca>
2023-06-18 23:01:23 -0700

    Update comments for mod tap settings

--- config.h
+++ config.h
@@ -1,2 +1,4 @@
+// Tap turns to hold after number of ms
 #define TAPPING_TERM 1000
+// https://github.com/qmk/qmk_firmware/blob/master/docs/tap_hold.md#tap-or-hold-decision-modes
 #define HOLD_ON_OTHER_KEY_PRESS
