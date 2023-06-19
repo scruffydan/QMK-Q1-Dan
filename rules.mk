cf03e376e5772d7edf92770aa8a709c2f563e863
Dan Moutal <dan@moutal.ca>
2023-06-18 22:56:39 -0700

    Add Mousekey for Karabiner compatability with mission control

--- rules.mk
+++ rules.mk
@@ -1,5 +1,6 @@
 # VIA_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
+MOUSEKEY_ENABLE = yes # needed to have Mission Control and Launch Pad working with Karabiner elements
 
 #VPATH += keyboards/keychron/common
 #SRC += keychron_common.c
